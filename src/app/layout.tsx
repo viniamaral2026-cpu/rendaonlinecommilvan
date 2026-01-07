eeb198428479c79098f9addfa2c418f2a22e5c4f
vini amaral <viniamaral2026@gmail.com>
2026-01-07 20:03:36 +0000

    AJUDA NOSSA LOG NO RODAPE NAO TA FIXA A BAIXO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       >
         <div className="flex flex-col min-h-screen">
           <Header />
-          <main className="flex-grow">{children}</main>
+          <main className="flex-1 flex flex-col">{children}</main>
           <Footer />
         </div>
         <Toaster />
